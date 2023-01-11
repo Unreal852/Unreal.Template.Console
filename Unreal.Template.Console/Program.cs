68d4c1868c42c9ec861f44519b271ce531992182
Unreal852 <unreal852@hotmail.fr>
2023-01-11 15:25:00 +0100

    Initial commit

---
+++ Unreal.Template.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+
+namespace Unreal.Template.Console;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        ConfigureLogger();
+
+        Log.Information("Hello world, the current date is {Date}", DateTime.Now);
+    }
+
+    private static void ConfigureLogger()
+    {
+        Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+    }
+}
